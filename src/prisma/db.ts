9dcfddb903339b124c485ffc926570e1a378ca29
aparties <fiestas.armando@gmail.com>
2026-08-25 09:53:39 -0500

    fix: fallback connection string during build time on Vercel

--- src/prisma/db.ts
+++ src/prisma/db.ts
@@ -3,7 +3,9 @@ import postgres from '@prisma/orm-postgres/runtime';
 import type { Contract } from './contract.d';
 import contractJson from './contract.json' with { type: 'json' };
 
+const dbUrl = process.env['DATABASE_URL'] || 'postgresql://postgres:postgres@localhost:5432/postgres';
+
 export const db = postgres<Contract>({
   contractJson,
-  url: process.env['DATABASE_URL']!,
+  url: dbUrl,
 });
